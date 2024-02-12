6dea204f569a199da1795105c13336e93cc3b1c6
Mukhriz <89246506+monkeMuk@users.noreply.github.com>
2024-02-12 15:50:21 +0800

    Add files via upload

---
+++ OOP/src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        Plane plane1 = new Plane();
+
+        System.out.println(plane1.model);
+        
+        plane1.descend();
+    }
+}
---
+++ OOP/src/Plane.java
@@ -0,0 +1,18 @@
+public class Plane {
+    
+    // attributes
+    String make = "Boeing";
+    String model = "B737";
+    int year = 2020;
+    String color = "blue";
+    double price = 20000.00;
+
+    // methods
+    void climb(){
+        System.out.println("You are climbing");
+    }
+
+    void descend(){
+        System.out.println("You at descending");
+    }
+}
